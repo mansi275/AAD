c774f1896967ccf7a338ccbc4dbad9c290c8fbf3
mansi275 <96366490+mansi275@users.noreply.github.com>
2021-12-20 01:24:01 +0530

    Update ArmstrongNumber.java

--- Assignment1/ArmstrongNumber.java
+++ Assignment1/ArmstrongNumber.java
@@ -1,34 +1,33 @@
 package com.mpstme;
 
 import java.util.Scanner;
+import java.lang.Math;
 
-public class NeonNumber {
+public class ArmstrongNumber {
 
     public static void main(String[] args){
         Scanner number = new Scanner(System.in);
         System.out.println("Enter a number: ");
         int num = number.nextInt();
-        System.out.println(num);       // 11
+        System.out.println(num);  // 153
 
-        int numSquare = num*num;       // 121
+        int sum = 0, len = 0;
+        int as_no = num;
 
-        int sum = 0;
-
-        while (numSquare>0){
-            sum = sum + numSquare%10;   //0+1=1
-            numSquare = numSquare/10;   //121/10=12.1 which makes it 12 as it is int
-            //step 2
-            //1=1+2=3  (new number is 12, so 12 modulus 10 is 2)
-            //12 by 10 = 1.2 = 1 (same step continues till number square is less than 0)
-            //step 3
-            //3=3+1=4  (1 modulus 10 = 1)
-            //1 by 10 = 0.1 = 0
-            //break
+        while (as_no>0){
+            ++len;          // 153 greater than 0, 0 ==> 1
+            as_no=as_no/10; // 153/10 = 15 (int)
         }
+        as_no=num;
+        while (as_no>0){
+            sum+=Math.pow(as_no%10,len);  // 0 + 3^3
+            as_no=as_no/10;               // 153/10 = 15
+        }
+
         if (num==sum){
-            System.out.println("This is a Neon Number.");
+            System.out.println("This is an Armstrong Number.");
         } else  {
-            System.out.println("This is not a Neon Number.");   //sum turns out to be 4 != 121
+            System.out.println("This is not an Armstrong Number.");
         }
 
     }
