dee293c04d0fa7054e8f468cc168a962057811b2
mansi275 <96366490+mansi275@users.noreply.github.com>
2021-12-20 01:24:49 +0530

    Update NeonNumber.java

--- Assignment1/NeonNumber.java
+++ Assignment1/NeonNumber.java
@@ -8,18 +8,27 @@ public class NeonNumber {
         Scanner number = new Scanner(System.in);
         System.out.println("Enter a number: ");
         int num = number.nextInt();
-        System.out.println(num);
+        System.out.println(num);       // 11
 
-        int numSquare = num*num;
+        int numSquare = num*num;       // 121
+
         int sum = 0;
+
         while (numSquare>0){
-            sum+=numSquare%10; //sum+= ==> sum + numSquare%10
-            numSquare=numSquare/10;
+            sum = sum + numSquare%10;   //0+1=1
+            numSquare = numSquare/10;   //121/10=12.1 which makes it 12 as it is int
+            //step 2
+            //1=1+2=3  (new number is 12, so 12 modulus 10 is 2)
+            //12 by 10 = 1.2 = 1 (same step continues till number square is less than 0)
+            //step 3
+            //3=3+1=4  (1 modulus 10 = 1)
+            //1 by 10 = 0.1 = 0
+            //break
         }
         if (num==sum){
             System.out.println("This is a Neon Number.");
         } else  {
-            System.out.println("This is not a Neon Number.");
+            System.out.println("This is not a Neon Number.");   //sum turns out to be 4 != 121
         }
 
     }
